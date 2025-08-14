11e3ac515ef608e459759e272a17d1073fb65e62
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-08-14 15:28:46 +0800

    feat: Add greeting message

--- src/main/java/SmartOtter.java
+++ src/main/java/SmartOtter.java
@@ -1,10 +1,15 @@
 public class SmartOtter {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatBotName = "SmartOtter";
+        String greetingMessage = String.format("""
+                ____________________________________________________________
+                 Hello! I'm %s
+                 What can I do for you?
+                ____________________________________________________________
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                
+                """, chatBotName);
+        System.out.println(greetingMessage);
     }
 }
