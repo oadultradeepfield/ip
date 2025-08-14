c38c535dbf3e50198c310f540a6ceaea4d90e8a7
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-08-14 20:14:57 +0800

    feat: Extend task to have status

--- src/main/java/com/oadultradeepfield/smartotter/task/Task.java
+++ src/main/java/com/oadultradeepfield/smartotter/task/Task.java
@@ -2,13 +2,27 @@ package com.oadultradeepfield.smartotter.task;
 
 public class Task {
     private final String taskName;
+    private boolean isDone;
 
-    public Task(String taskName) {
+    public Task(String taskName, boolean isDone) {
         this.taskName = taskName;
+        this.isDone = isDone;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void markAsDone() {
+        isDone = true;
     }
 
     @Override
     public String toString() {
-        return taskName;
+        if (isDone) {
+            return "[X] " + taskName;
+        } else {
+            return "[ ] " + taskName;
+        }
     }
 }
